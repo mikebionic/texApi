7a8b361d1eb8dd006967b886eae10ae6b1e8d77c
Muhammetberdi Jepbarov <muhammedjepbarov@gmail.com>
2024-09-24 20:49:05 +0500

    update in content  type

--- internal/dto/content_type.go
+++ internal/dto/content_type.go
@@ -10,3 +10,12 @@ type ContentType struct {
 	Description string            `json:"description"`
 	ContentData []ContentResponse `json:"content_data"`
 }
+
+type ContentTypeWithContent struct {
+	//ID          int               `db:"content_type_id" json:"id"`
+	UUID        uuid.UUID         `db:"content_type_uuid" json:"uuid"`
+	Name        string            `db:"content_type_name" json:"name"`
+	Title       string            `db:"content_type_title" json:"title"`
+	Description string            `db:"content_type_description" json:"description"`
+	Contents    []ContentResponse `json:"contents"`
+}
