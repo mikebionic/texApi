0986982742f8db645e0eca28c9a83df2ee35d9ab
Muhammetberdi Jepbarov <muhammedjepbarov@gmail.com>
2024-10-02 12:03:56 +0500

    optimize sql to join request

--- internal/repositories/content_type.go
+++ internal/repositories/content_type.go
@@ -8,44 +8,20 @@ import (
 	"texApi/internal/queries"
 )
 
-func GetContentTypes(withContent int) ([]dto.ContentType, error) {
-	var contentTypes []dto.ContentType
+func GetContentTypes(withContent int) ([]dto.ContentTypeWithContents, error) {
+	var contentTypes []dto.ContentTypeWithContents
+
 	if withContent > 0 {
-		var results []struct {
-			ContentType dto.ContentTypeWithContent
-			Content     dto.ContentResponse
-		}
-		err := pgxscan.Select(context.Background(), db.DB, &results, queries.GetContentTypesWithContent)
+		err := pgxscan.Select(context.Background(), db.DB, &contentTypes, queries.GetContentTypesWithContent)
 		if err != nil {
 			return nil, err
 		}
-
-		//contentTypesMap := make(map[int]*dto.ContentTypeWithContent)
-		//for _, row := range results {
-		//	fmt.Println(row)
-		//	if _, exists := contentTypesMap[row.ContentType.ID]; !exists {
-		//		contentTypesMap[row.ContentType.ID] = &row.ContentType
-		//		contentTypesMap[row.ContentType.ID].Contents = []dto.ContentResponse{}
-		//	}
-		//
-		//	if row.Content.ID != 0 {
-		//		contentTypesMap[row.ContentType.ID].Contents = append(
-		//			contentTypesMap[row.ContentType.ID].Contents,
-		//			row.Content,
-		//		)
-		//	}
-		//}
-
-		//for _, ct := range contentTypesMap {
-		//	contentTypes = append(contentTypes, *ct)
-		//}
 	} else {
-		err := pgxscan.Select(
-			context.Background(), db.DB,
-			&contentTypes, queries.GetContentTypes)
+		err := pgxscan.Select(context.Background(), db.DB, &contentTypes, queries.GetContentTypes)
 		if err != nil {
 			return nil, err
 		}
 	}
+
 	return contentTypes, nil
 }
