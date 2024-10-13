5a5e137025745c247f20f1a37788f726177adee8
Muhammetberdi Jepbarov <muhammedjepbarov@gmail.com>
2024-10-13 18:45:45 +0500

    handling empty values for update

--- internal/queries/content.go
+++ internal/queries/content.go
@@ -61,22 +61,22 @@ var CreateContent = `
     `
 
 var UpdateContent = `
-    UPDATE tbl_content
-    SET
-        lang_id = $1,
-        content_type_id = $2,
-        title = $3,
-        slogan = $4,
-        subtitle = $5,
-        description = $6,
-        count = $7,
-        count_type = $8,
-        image_url = $9,
-        video_url = $10,
-        step = $11,
-        active = $12
-    WHERE id = $13
-    RETURNING id
+	UPDATE tbl_content
+	SET
+		lang_id = COALESCE($1, lang_id),
+		content_type_id = COALESCE($2, content_type_id),
+		title = COALESCE($3, title),
+		slogan = COALESCE($4, slogan),
+		subtitle = COALESCE($5, subtitle),
+		description = COALESCE($6, description),
+		count = COALESCE($7, count),
+		count_type = COALESCE($8, count_type),
+		image_url = COALESCE($9, image_url),
+		video_url = COALESCE($10, video_url),
+		step = COALESCE($11, step),
+		active = COALESCE($12, active)
+	WHERE id = $13
+	RETURNING id
 `
 
 var DeleteContent = `
