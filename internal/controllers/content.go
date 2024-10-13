1bcc783c2fb822eb95a0fb97f20fdbe556bb6802
Muhammetberdi Jepbarov <muhammedjepbarov@gmail.com>
2024-10-13 18:13:51 +0500

    added one more route for update

--- internal/controllers/content.go
+++ internal/controllers/content.go
@@ -12,6 +12,7 @@ func Content(router *gin.Engine) {
 	group.GET("/:id", services.GetContent)
 	group.POST("", services.CreateContent)
 	group.PUT("/:id", services.UpdateContent)
+	group.POST("/update/:id", services.UpdateContent)
 	group.DELETE("/:id", services.DeleteContent)
 
 }
