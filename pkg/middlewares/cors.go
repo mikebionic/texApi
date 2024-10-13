ca470ebf2d31cc0f59cda130c79062dd0a31a6d1
Muhammetberdi Jepbarov <muhammedjepbarov@gmail.com>
2024-10-14 03:18:48 +0500

    cors update

--- pkg/middlewares/cors.go
+++ pkg/middlewares/cors.go
@@ -5,8 +5,10 @@ import "github.com/gin-gonic/gin"
 func Cors(ctx *gin.Context) {
 	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-	ctx.Writer.Header().Set("Access-Control-Allow-Headers", "*")
-	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "*")
+	ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, RoleID, Credentials, RegisterMethod, OTP, Authorization")
+	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
+	ctx.Writer.Header().Set("AllowFiles", "*")
+	ctx.Writer.Header().Set("AllowCredentials", "*")
 
 	if ctx.Request.Method == "OPTIONS" {
 		ctx.AbortWithStatus(204)
