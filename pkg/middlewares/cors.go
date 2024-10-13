207843262c8e1f99448f85700944f4b118866499
Muhammetberdi Jepbarov <muhammedjepbarov@gmail.com>
2024-10-14 03:03:12 +0500

    cors update

--- pkg/middlewares/cors.go
+++ pkg/middlewares/cors.go
@@ -3,7 +3,7 @@ package middlewares
 import "github.com/gin-gonic/gin"
 
 func Cors(ctx *gin.Context) {
-	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	ctx.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "*")
