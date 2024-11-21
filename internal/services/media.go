6223f2769af047183fa3b70d6a076b8ead234bc2
mikebionic <muhammedjepbarov@gmail.com>
2024-11-21 12:46:22 +0500

    update cors

--- internal/services/media.go
+++ internal/services/media.go
@@ -3,6 +3,7 @@ package services
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"texApi/config"
 	"texApi/pkg/utils"
 )
 
@@ -12,6 +13,10 @@ func UploadFile(ctx *gin.Context) {
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, utils.FormatErrorResponse("Error saving file", err.Error()))
 		return
+	}
+
+	for k, filePath := range filePaths {
+		filePaths[k] = config.ENV.API_SERVER_URL + filePath
 	}
 
 	ctx.JSON(http.StatusCreated, utils.FormatResponse("Successfully uploaded", filePaths))
