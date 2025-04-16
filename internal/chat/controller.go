ab10ef4119ad89631297547738c86f7a6b6129f9
mikebionic <muhammedjepbarov@gmail.com>
2025-04-16 09:43:19 +0500

    add / in chat routes

--- internal/chat/controller.go
+++ internal/chat/controller.go
@@ -21,29 +21,29 @@ func Chat(router *gin.Engine) {
 	convGroup := group.Group("/conversations/:id", apiHandler.ConversationAccessMiddleware())
 	{
 		convGroup.GET("/", apiHandler.GetConversation)
-		convGroup.GET("/pinned", apiHandler.GetPinnedMessages)
-		convGroup.POST("/pin", apiHandler.PinMessage)
+		convGroup.GET("/pinned/", apiHandler.GetPinnedMessages)
+		convGroup.POST("/pin/", apiHandler.PinMessage)
 		convGroup.GET("/message/", apiHandler.GetMessages)
 		convGroup.POST("/message/", apiHandler.SendMessage)
 		convGroup.PUT("/message/", apiHandler.EditMessage)
 		convGroup.DELETE("/message/", apiHandler.DeleteMessage)
-		convGroup.POST("/message/react", apiHandler.ReactToMessage)
+		convGroup.POST("/message/react/", apiHandler.ReactToMessage)
 	}
-	group.GET("/conversations", apiHandler.GetConversations)
-	group.POST("/conversations", apiHandler.CreateConversation)
-	group.GET("/search", apiHandler.SearchMessages)
+	group.GET("/conversations/", apiHandler.GetConversations)
+	group.POST("/conversations/", apiHandler.CreateConversation)
+	group.GET("/search/", apiHandler.SearchMessages)
 
 	wsHandler := NewWebSocketHandler(ChatHub, chatRepository, jwtSecret)
 	wsRouteGroup := router.Group(config.ENV.API_PREFIX + "/ws/")
 	wsRouteGroup.Use(middlewares.Guard)
 
-	wsRouteGroup.GET("/connect",
+	wsRouteGroup.GET("/connect/",
 		func(c *gin.Context) { log.Printf("WebSocket connection attempt from %s", c.ClientIP()) },
 		wsHandler.HandleWebSocket)
-	wsRouteGroup.GET("/join",
+	wsRouteGroup.GET("/join/",
 		func(c *gin.Context) { log.Printf("Conversation join attempt from %s", c.ClientIP()) },
 		wsHandler.HandleJoinConversation)
-	wsRouteGroup.GET("/leave",
+	wsRouteGroup.GET("/leave/",
 		func(c *gin.Context) { log.Printf("Conversation leave attempt from %s", c.ClientIP()) },
 		wsHandler.HandleLeaveConversation)
 }
